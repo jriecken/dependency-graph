e22e2b18ec04ee3cd7cb6a257264c16b15ecf5f0
Andrew Healey <doolie.healey@gmail.com>
2023-12-02 15:22:33 +0000

    Add tests for circular cloning behavior

--- specs/dep_graph_spec.js
+++ specs/dep_graph_spec.js
@@ -382,6 +382,18 @@ describe("DepGraph", function() {
     expect(graph === cloned).toBeFalse();
   });
 
+  it("should preserve the circular property on clone", function() {
+    var graph = new DepGraph({ circular: true });
+    expect(graph.circular).toBeTrue();
+    var cloned = graph.clone();
+    expect(cloned.circular).toBeTrue();
+
+    var graph2 = new DepGraph({ circular: false });
+    expect(graph2.circular).toBeFalse();
+    var cloned2 = graph2.clone();
+    expect(cloned2.circular).toBeFalse();
+  });
+
   it("should clone a non-empty graph", function() {
     var graph = new DepGraph();
 
